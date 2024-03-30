3ab1a140aebf4c13da175c6fed41e355d2a656d7
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-03-30 11:06:59 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import HomePage from './pages/HomePage.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</style>./pages/HomePage.vue
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-slate-500 w-4 h-4">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
