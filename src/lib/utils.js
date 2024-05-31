f6d912677d0e872d973dfed77f55a84c601bfc5a
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-05-31 08:37:42 +0700

    library

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,16 @@
+import { ref } from "vue";
+
+const dateToday = ref("");
+const today = new Date();
+const day = String(today.getDate()).padStart(2, "0");
+const month = String(today.getMonth() + 1).padStart(2, "0"); // January is 0!
+const year = today.getFullYear();
+dateToday.value = `${year}-${month}-${day}`;
+
 export function formatPrice(price) {
   return price.toLocaleString("id-ID");
 }
+
+export const currentDate = dateToday
+export const currentMonth = month
+export const currentYear = year
---
+++ src/lib/voucher.js
@@ -0,0 +1,102 @@
+export const voucher = [
+  {
+    brand: "ALFAMART VOUCHER",
+    keterangan: "ALFAMART VOUCHER",
+  },
+  {
+    brand: "Battle Net Gift Card",
+    keterangan: "Battle Net Gift Card",
+  },
+  {
+    brand: "Dota Auto Chess Candy (Global)",
+    keterangan: "Dota Auto Chess Candy (Global)",
+  },
+  {
+    brand: "Game-On Credits",
+    keterangan: "Game-On Credits",
+  },
+  {
+    brand: "GARENA",
+    keterangan: "GARENA",
+  },
+  {
+    brand: "GOOGLE PLAY INDONESIA",
+    keterangan: "GOOGLE PLAY INDONESIA",
+  },
+  {
+    brand: "GOOGLE PLAY US REGION",
+    keterangan: "GOOGLE PLAY US REGION",
+  },
+  {
+    brand: "IMVU",
+    keterangan: "IMVU",
+  },
+  {
+    brand: "INDOMARET",
+    keterangan: "INDOMARET",
+  },
+  {
+    brand: "iTunes",
+    keterangan: "iTunes",
+  },
+  {
+    brand: "Karma Koin",
+    keterangan: "Karma Koin",
+  },
+  {
+    brand: "MEGAXUS",
+    keterangan: "MEGAXUS",
+  },
+  {
+    brand: "Nintendo eShop",
+    keterangan: "Nintendo eShop",
+  },
+  {
+    brand: "PLAYSTATION",
+    keterangan: "PLAYSTATION",
+  },
+  {
+    brand: "Playwith",
+    keterangan: "Playwith",
+  },
+  {
+    brand: "POINT BLANK",
+    keterangan: "POINT BLANK",
+  },
+  {
+    brand: "Razer Gold",
+    keterangan: "Razer Gold",
+  },
+  {
+    brand: "Riot Cash",
+    keterangan: "Riot Cash",
+  },
+  {
+    brand: "Roblox",
+    keterangan: "Roblox",
+  },
+  {
+    brand: "SPOTIFY",
+    keterangan: "SPOTIFY",
+  },
+  {
+    brand: "Steam Wallet (IDR)",
+    keterangan: "Steam Wallet (IDR)",
+  },
+  {
+    brand: "Unipin Voucher",
+    keterangan: "Unipin Voucher",
+  },
+  {
+    brand: "Vidio",
+    keterangan: "Vidio",
+  },
+  {
+    brand: "WIFI ID",
+    keterangan: "WIFI ID",
+  },
+  {
+    brand: "XBOX",
+    keterangan: "XBOX",
+  },
+];
