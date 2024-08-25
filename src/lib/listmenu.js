41eea037bedabe4522aa7412dd701abd3d5df460
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-08-25 11:33:54 +0700

    list menu

--- src/lib/listmenu.js
+++ src/lib/listmenu.js
@@ -2,36 +2,43 @@ export const menu = [
     {
         "id": 1,
         "name" : "Pulsa",
-        "path": "/home/pulsa"
+        "path": "/home/pulsa",
+        "img": require("../assets/icons/menu/Pulsa.png")
     },
     {
         "id": 2,
         "name" : "Paket Data",
-        "path": "/home/paket-data"
+        "path": "/home/paket-data",
+        "img": require("../assets/icons/menu/PaketData.png")
     },
     {
         "id": 3,
         "name" : "Games",
-        "path": "/home/games"
+        "path": "/home/games",
+        "img": require("../assets/icons/menu/Game.png")
     },
     {
         "id": 4,
         "name" : "E Wallet",
-        "path": "/home/e-wallet"
+        "path": "/home/e-wallet",
+        "img": require("../assets/icons/menu/E-Money.png")
     },
     {
         "id": 5,
         "name" : "PLN",
-        "path": "/home/tagihan"
+        "path": "/home/tagihan",
+        "img": require("../assets/icons/menu/PLN.png")
     },
     {
         "id": 6,
         "name" : "Voucher",
-        "path": "/home/voucher"
+        "path": "/home/voucher",
+        "img": require("../assets/icons/menu/Voucher.png")
     },
     {
         "id": 7,
         "name" : "Social Media",
-        "path": "/home/social-media"
+        "path": "/home/social-media",
+        "img": require("../assets/icons/menu/SocialMedia.png")
     },
 ]
