41694200e7bcb669db9bc3b3211700d119a93106
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-08-25 15:28:55 +0700

    minor fix

--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -1,14 +1,11 @@
 <script setup>
-// import AppHeader from "./AppHeader.vue";
-import SideBar from "./SideBar.vue";
+import AppSidebar from "./AppSidebar.vue";
 import MainContent from "./MainContent.vue";
 import AppFooter from "./AppFooter.vue";
 
-// import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 import { computed, ref, watchEffect } from "vue";
 
-// const store = useStore();
 const route = useRoute();
 const routeName = ref(route.name);
 watchEffect(() => (routeName.value = route.name));
@@ -18,15 +15,12 @@ const loginRoute = computed(
 </script>
 <template>
   <div class="w-full h-auto min-h-screen flex flex-col gap-0 bg-[#EDF0F2]">
-    <SideBar v-if="!loginRoute" />
-    <!-- <div class="w-full h-auto min-h-screen flex flex-col gap-0 bg-[#EDF0F2]"> -->
-    <!-- <AppHeader v-if="!loginRoute" /> -->
+    <AppSidebar v-if="!loginRoute" />
     <main class="flex flex-col">
       <MainContent>
         <slot></slot>
       </MainContent>
       <AppFooter />
     </main>
-    <!-- </div> -->
   </div>
 </template>
