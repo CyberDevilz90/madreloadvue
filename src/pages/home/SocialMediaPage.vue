24fedecdd934089e982769f8f80e4ce7f28cfe9b
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-08-27 08:58:23 +0700

    checkout function

--- src/pages/home/SocialMediaPage.vue
+++ src/pages/home/SocialMediaPage.vue
@@ -7,7 +7,10 @@ import Loader from "@/components/ui/Loader.vue";
 import SocialMediaRule from "../../components/ui/SocialMediaRule.vue";
 import VueSelect from "vue3-select";
 import "vue3-select/dist/vue3-select.css";
+import router from "@/router";
+import { useAuthStore } from "@/store/modules/auth";
 
+const authStore = useAuthStore()
 const isLoading = ref(false);
 const listSocialMedia = ref([]);
 const selectedCategory = ref("");
@@ -16,7 +19,7 @@ const selectedProducts = ref([]);
 const listCategories = ref([]);
 const commentInput = ref("");
 const totalPrice = ref(null);
-const jumlahInput = ref("");
+const jumlahInput = ref(0);
 const userId = ref("");
 
 const getPriceListPPOB = async () => {
@@ -72,6 +75,37 @@ function calculateTotalPrice() {
   }
 }
 
+async function checkout() {
+  if (selectedProduct.value == "" || userId.value == ""){
+    alert("Silahkan pilih layanan dan isi Username / Link tujuan")
+  }
+
+  try {
+    isLoading.value = true
+    const apiUrl = `${process.env.VUE_APP_BE_API_URL}/transactions/create-order-ssm`
+    const payload = {
+      user_id: `${authStore.user.id}`,
+      service: selectedProduct.value,
+      destination: userId.value,
+      quantity: jumlahInput.value,
+      price: totalPrice.value,
+    }
+    console.log(payload)
+    //eslint-disable-next-line
+    const response = await axios.post(apiUrl, payload)
+    isLoading.value = false
+    router.go(-1)
+  } catch (error){
+    if (error.response && error.response.data.error === "Insufficient balance") {
+      alert("Saldo tidak cukup, Silahkan Top Up saldo telebih dahulu")
+      isLoading.value = false
+    } else {
+      console.error("Error performing transaction", error)
+      isLoading.value = false
+    }
+  }
+}
+
 watch(selectedCategory, filterProducts);
 watch(
   () => [selectedProduct.value, jumlahInput.value],
@@ -93,31 +127,7 @@ onMounted(() => {
     <Loader width="100px" height="100px" />
   </div>
   <div class="w-full p-5 mb-5">
-    <div class="flex justify-center gap-3 p-5 card">
-      <input
-        class="w-1/3 p-[8px] rounded-md"
-        type="text"
-        placeholder="Username / Link"
-        v-model="userId"
-      />
-      <input
-        class="w-1/3 p-[8px] rounded-md"
-        type="text"
-        placeholder="Jumlah"
-        v-model.lazy="jumlahInput"
-        :min="
-          selectedProduct
-            ? selectedProducts.find((p) => p.id === selectedProduct).min
-            : ''
-        "
-        :max="
-          selectedProduct
-            ? selectedProducts.find((p) => p.id === selectedProduct).max
-            : ''
-        "
-      />
-    </div>
-    <div class="flex flex-col justify-center pt-5 mt-5 card">
+    <div class="flex flex-col justify-center pt-5 card">
       <p>Category :</p>
       <vue-select
         :options="listCategories"
@@ -141,6 +151,32 @@ onMounted(() => {
           {{ product.name }}
         </option>
       </select>
+    </div>
+    <div class="flex items-center justify-center gap-3 p-5 mt-5 card">
+      <p>Target :</p>
+      <input
+        class="w-1/3 p-[8px] rounded-md"
+        type="text"
+        placeholder="Username / Link"
+        v-model="userId"
+      />
+      <p>Jumlah :</p>
+      <input
+        class="w-1/3 p-[8px] rounded-md"
+        type="number"
+        placeholder="Jumlah"
+        v-model="jumlahInput"
+        :min="
+          selectedProduct
+            ? selectedProducts.find((p) => p.id === selectedProduct).min
+            : ''
+        "
+        :max="
+          selectedProduct
+            ? selectedProducts.find((p) => p.id === selectedProduct).max
+            : ''
+        "
+      />
     </div>
     <div
       v-if="
@@ -223,7 +259,7 @@ onMounted(() => {
     <p v-if="totalPrice !== null" class="text-center">
       Total = {{ formatPrice(totalPrice) }}
     </p>
-    <div
+    <div @click="checkout"
       class="w-full p-[8px] mt-5 mb-5 text-center bg-green-600 rounded-md hover:cursor-pointer"
     >
       <p class="font-bold text-white">Checkout</p>
