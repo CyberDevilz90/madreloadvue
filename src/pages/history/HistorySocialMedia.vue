8bdef2bcc6c2fed26b9b8c68b3afab63fa406ec5
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-08-27 08:58:02 +0700

    copy button

--- src/pages/history/HistorySocialMedia.vue
+++ src/pages/history/HistorySocialMedia.vue
@@ -24,6 +24,10 @@ const formatDate = (dateString) => {
   return new Date(dateString).toLocaleDateString("en-US", options);
 };
 
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text);
+};
+
 onMounted(() => {
   getHistorySMM();
 });
@@ -33,7 +37,7 @@ onMounted(() => {
     <div v-if="error">{{ error }}</div>
     <div v-else>
       <table class="w-full">
-        <tr class="bg-white">
+        <tr class="h-10 bg-white">
           <th>ID Trx</th>
           <th>Nama Layanan</th>
           <th>Tujuan / Target</th>
@@ -49,26 +53,36 @@ onMounted(() => {
           :key="item.id"
           class="p-2 m-4 bg-slate-50"
         >
-          <th class="w-[5%]">{{ item.id }}</th>
-          <th class="text-center w-[40%]">{{ item.service_name }}</th>
-          <th class="text-center w-[15%]">{{ item.data }}</th>
-          <th class="text-center w-[15%]">{{ item.quantity }}</th>
-          <th class="text-center w-[15%]">{{ formatPrice(item.price) }}</th>
-          <th class="text-center w-[15%]">{{ item.start_count }}</th>
-          <th class="text-center w-[15%]">{{ item.remains }}</th>
+          <th class="w-[8%]">{{ item.id }}</th>
+          <th class="text-center w-[20%]">{{ item.service_name }}</th>
+          <th class="text-center max-w-[30%] truncate">
+            {{ item.data }}
+            <button
+              @click="copyToClipboard(item.data)"
+              class="p-1 ml-2 text-xs text-white bg-blue-500 rounded"
+            >
+              Copy
+            </button>
+          </th>
+          <th class="text-center w-[7%]">{{ item.quantity }}</th>
+          <th class="text-center w-[7%]">{{ formatPrice(item.price) }}</th>
+          <th class="text-center w-[7%]">{{ item.start_count }}</th>
+          <th class="text-center w-[7%]">{{ item.remains }}</th>
           <th
             :class="[
-              'text-center w-[15%] text-white',
-              item.status == 'pending'
+              'text-center w-[7%] text-white',
+              item.status == 'Pending' || item.status == 'pending'
                 ? 'bg-yellow-300'
-                : item.status == 'Success'
+                : item.status == 'Success' || item.status == 'success'
                 ? 'bg-green-500'
-                : 'bg-red-500'
+                : item.status == 'In progress' || item.status == 'in progress'
+                ? 'bg-blue-500'
+                : 'bg-red-500',
             ]"
           >
             {{ item.status }}
           </th>
-          <th class="text-center w-[15%]">
+          <th class="text-center w-[10%]">
             {{ formatDate(item.tanggal_order) }}
           </th>
         </tr>
@@ -76,3 +90,4 @@ onMounted(() => {
     </div>
   </div>
 </template>
+<style scoped></style>
