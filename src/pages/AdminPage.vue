9a7c89dacf443e273c7d51f27914c5cca997f3cf
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2025-01-31 05:46:13 +0700

    add Loading screen when still fetching data

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -2,9 +2,11 @@
 import axios from "axios";
 import { onMounted, ref } from "vue";
 import { formatPrice } from "@/lib/utils";
+import Loader from "@/components/ui/Loader.vue";
 
 const balanceBuzzerpanel = ref(0);
 const balanceDigiflazz = ref(0);
+const isLoading = ref(true);
 
 async function buzzerBalance() {
   const urlBuzzerpanel = "http://127.0.0.1:5000/utils/admin-buzzer";
@@ -25,52 +27,53 @@ async function digiflazzBalance() {
   }
 }
 
-onMounted(() => {
-  buzzerBalance();
-  digiflazzBalance();
+onMounted( async() => {
+  await Promise.all([buzzerBalance(), digiflazzBalance()]);
+  isLoading.value = false; 
 });
 </script>
 
 <template>
-  <div class="flex flex-col items-center m-3">
-    <div class="flex">
+  <Loader v-if="isLoading" />
+  <div v-else class="flex flex-col items-center m-3 my-16">
+    <div class="flex justify-start">
       <img src="../assets/icons/buzzerpanel.png" class="w-8 h-8" />
       <p>Buzzerpanel : {{ formatPrice(parseInt(balanceBuzzerpanel)) }}</p>
     </div>
-    <div class="flex">
+    <div class="flex items-start">
       <img src="../assets/icons/digiflazz.png" class="w-6 h-6" />
       <p>DigiFlazz : {{ formatPrice(balanceDigiflazz) }}</p>
     </div>
   </div>
-  <div class="flex flex-row justify-center gap-2 p-5">
+  <div class="flex flex-row items-center justify-center gap-24 p-5 mb-16 min-h-48">
     <RouterLink to="/admin/set-margin">
       <img
-        class="w-[200px]"
+        class="w-[150px]"
         src="../assets/icons/profit.png"
         alt="logo"
       />
       <p class="text-center">Margin Omset</p>
     </RouterLink>
-    <RouterLink to="/admin/topup-manual">
+    <RouterLink to="/admin/manage-order">
       <img
-        class="w-[200px]"
-        src="../assets/images/madreloadIcon.png"
+        class="w-[150px]"
+        src="../assets/icons/clipboard.png"
         alt="logo"
       />
       <p class="text-center">Manage Order</p>
     </RouterLink>
     <RouterLink to="/admin/manage-user">
       <img
-        class="w-[200px]"
-        src="../assets/images/madreloadIcon.png"
+        class="w-[150px]"
+        src="../assets/icons/group.png"
         alt="logo"
       />
       <p class="text-center">Manage User</p>
     </RouterLink>
     <RouterLink to="/admin/topup-manual">
       <img
-        class="w-[200px]"
-        src="../assets/images/madreloadIcon.png"
+        class="w-[150px]"
+        src="../assets/icons/top-up.png"
         alt="logo"
       />
       <p class="text-center">Top Up Manual</p>
